c71de18d8c784580bb029708f55f4907e785e46f
Lani Villamor <lanivillamor@lanis-air.mynetworksettings.com>
2025-02-15 16:19:35 -0500

    navigate route to *

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); //all logic depends onwhether or not use is logged in
   return (
     <Routes>
       <Route path="/ducks" element={<Ducks />} />
       <Route path="/my-profile" element={<MyProfile />} />
+
       <Route
         path="/login"
         element={
@@ -24,6 +27,16 @@ function App() {
           <div className="registerContainer">
             <Register />
           </div>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
     </Routes>
