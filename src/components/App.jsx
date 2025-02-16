b8c41cec5506dea15232f13dfc63b2f3da27b0b1
Lani Villamor <lanivillamor@Lanis-MacBook-Air.local>
2025-02-16 11:20:57 -0500

    Added ProtectedRoute wrapper component to protect routes ducks and MyProfile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,28 @@ import MyProfile from "./MyProfile";
 import Register from "./Register";
 import "./styles/App.css";
 import { useState } from "react";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); //all logic depends onwhether or not use is logged in
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
+      <Route
+        path="/ducks"
+        element={
+          <ProtectedRoute>
+            <Ducks />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/my-profile"
+        element={
+          <ProtectedRoute>
+            <MyProfile />
+          </ProtectedRoute>
+        }
+      />
 
       <Route
         path="/login"
